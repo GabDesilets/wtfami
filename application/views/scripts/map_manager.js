eeea09e316bae056ddf49d4de692f2d82373cdaa
jfgelinas4 <jfgelinas4@gmail.com>
2016-01-31 12:11:41 -0500

    Some sexy stuff

--- application/views/scripts/map_manager.js
+++ application/views/scripts/map_manager.js
@@ -5,7 +5,6 @@ var poly;
 var road = [];
 var pointsOfInterest = [];
 var roadIsComplete = false;
-
 function initView() {
   if (routeName != "") {
     initMap();
@@ -13,17 +12,14 @@ function initView() {
     $('#routeInfoModal').openModal();
   }  
 }
-
 function initMap() {
   var mapOptions = {
     zoom: 15,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
-
   map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
   poly = new google.maps.Polyline({ strokeColor: '#000000 ', strokeOpacity: 1.0, strokeWeight: 3 });
   poly.setMap(map);
-
   // Add a listener for the click event
   map.addListener('click', onMapClick);
   
@@ -35,9 +31,7 @@ function initMap() {
     }, function() {
       handleNoGeolocation(true);
     });
-
     var contentString = '<div id="content"><div id="siteNotice"></div></div>';
-
     var infowindow = new google.maps.InfoWindow({
       content: contentString
     });
@@ -57,10 +51,8 @@ function initMap() {
       $('#titleDiv h5').prop('outerHTML') + "<br/><br/><a id='roadDoneButton' class='waves-effect waves-light btn' onclick='completeRoad()'>Ma route est prête!</a>";
     }
   }
-
   compilePointsOfInterest();
 }
-
 function updateRouteInfo() {
   routeName = $("#route-name").val();
   $('#route-name-disp')[0].innerHTML = routeName;
@@ -68,20 +60,15 @@ function updateRouteInfo() {
   $('#route-desc-disp')[0].innerHTML = routeDescription;
   initMap();
 }
-
 // Handles click events on a map, and adds a new point to the Polyline.
 function onMapClick(event) {
   if (editMode) {
     currentMarker = event.latLng;
-
     if (!roadIsComplete) {
       var path = poly.getPath();
-
        // Because path is an MVCArray, we can simply append a new coordinate and it will automatically appear.
       path.push(currentMarker);
-
       road.push(currentMarker);
-
       switch (road.length) {
         case 1: $('#titleDiv h5')[0].innerHTML = "Choisi un autre point pour tracer la route!"; break;
         default:
@@ -97,32 +84,51 @@ function onMapClick(event) {
     }
   }
 }
-
 function addMarker() {
   var markerName = $('#marker-name')[0].value;
   var markerDescription = $('#marker-desc')[0].value;
   $('#marker-name')[0].value = '';
   $('#marker-desc')[0].value = '';
-
   // Add a new marker at the new plotted point on the polyline.
   var marker = new google.maps.Marker({
     position: lastClickPosition,
     title: markerName,
     map: map,
     draggable: true,
+    animation: google.maps.Animation.DROP,
     markerRefNum: pointsOfInterest.length + 1,
     markerName: markerName,
   });
-
   pointsOfInterest.push({'marker': marker, 'name': markerName, 'description': markerDescription});
-
+  var contentString;
+  var infowindow;
+  marker.addListener('dragend', function() {
+    //console.log("HI");
+    //debugger;
+    contentString ='<div id="content"><h6>Point d\'int&eacute;r&ecirc;t #' + this.markerRefNum + '</h6><a onclick="deleteMarker(' + this.position.lat() + ', ' + this.position.lng() + ')"><i class="small material-icons" style="position: absolute;top: 1px;right: 15px;font-size: 13px;">delete</i></a>Name: ' + this.markerName + '<br>Location : (' + this.position.lat().toFixed(4) + ', ' + this.position.lng().toFixed(4) + ')</div>';
+    infowindow = new google.maps.InfoWindow({
+      content: contentString
+    });
+    infowindow.open(map, this);
+  });
   marker.addListener('click', function() {
-    var contentString ='<div id="content"><h6>Point d\'int&eacute;r&ecirc;t #' + this.markerRefNum + '</h6><a onclick="deleteMarker(' + this.position.lat() + ', ' + this.position.lng() + ')"><i class="small material-icons" style="position: absolute;top: 1px;right: 15px;font-size: 13px;">delete</i></a>Name: ' + this.markerName + '<br>Location : (' + this.position.lat().toFixed(4) + ', ' + this.position.lng().toFixed(4) + ')</div>';
-    var infowindow = new google.maps.InfoWindow({
+    toggleBounce(this);
+    contentString ='<div id="content"><h6>Point d\'int&eacute;r&ecirc;t #' + this.markerRefNum + '</h6><a onclick="deleteMarker(' + this.position.lat() + ', ' + this.position.lng() + ')"><i class="small material-icons" style="position: absolute;top: 1px;right: 15px;font-size: 13px;">delete</i></a>Name: ' + this.markerName + '<br>Location : (' + this.position.lat().toFixed(4) + ', ' + this.position.lng().toFixed(4) + ')</div>';
+    //g.innerHTML='<h6>Point d\'int&eacute;r&ecirc;t #' + this.markerRefNum + '</h6><a onclick="deleteMarker(' + this.position.lat() + ', ' + this.position.lng() + ')"><i class="small material-icons" style="position: absolute;top: 1px;right: 15px;font-size: 13px;">delete</i></a>Name: ' + this.markerName + '<br>Location : (' + this.position.lat().toFixed(4) + ', ' + this.position.lng().toFixed(4) ;
+    infowindow = new google.maps.InfoWindow({
        content: contentString
     });
     infowindow.open(map, this);
   });
+}
+
+function toggleBounce(markerToBounce) {
+  console.log('I was here');
+  if (markerToBounce.getAnimation() !== null) {
+    markerToBounce.setAnimation(null);
+  } else {
+    markerToBounce.setAnimation(google.maps.Animation.BOUNCE);
+  }
 }
 
 function deleteMarker(lat, lng) {
@@ -137,19 +143,16 @@ function deleteMarker(lat, lng) {
   }
   //compilePointsOfInterest();
 }
-
 function deleteRoadPoint(idx) {
   road.splice(idx,1);
   poly.setPath(road);
 }
-
 function completeRoad() {
   $('#titleDiv h5')[0].innerHTML = "Maintenant que ta route est prête, tu peux ajouter autant de points d'intérêt que tu le veux, ou simplement en rester là.";
   $('#roadDoneButton')[0].innerHTML = "Enregistrer";
   $("#roadDoneButton").attr("onclick","completeWTFAMI()");
   roadIsComplete = true;
 }
-
 function completeWTFAMI() {
   var road_markers = [];
   var pois = [];
@@ -161,8 +164,6 @@ function completeWTFAMI() {
         }
     );
   }
-
-  console.log(pointsOfInterest);
   for(var i = 0; i < pointsOfInterest.length; i++) {
     pois.push(
         {
@@ -172,7 +173,6 @@ function completeWTFAMI() {
             long: pointsOfInterest[i].marker.position.lng()
         }
     );
-
   }
   var routes = {
     'routes_markers': road_markers,
@@ -181,7 +181,6 @@ function completeWTFAMI() {
     'route_name': routeName,
     'route_description': routeDescription
   };
-
   $.ajax({
     type:    'POST',
     url:     $('#uglyurlpatch').val(),
@@ -192,7 +191,6 @@ function completeWTFAMI() {
     }
   });
 }
-
 function compileRoad() {
   var path = poly.getPath();
   nbRoadPoints = road.length;
@@ -201,7 +199,6 @@ function compileRoad() {
     path.push(road[i]);
   }
 }
-
 function compilePointsOfInterest() {
   //setMapOnAll(null);
   nbPointsOfInterest = pointsOfInterest.length;
@@ -218,10 +215,20 @@ function compilePointsOfInterest() {
         markerName: descriptionMarkers[i].name,
         map: map,
       });
-
       pointsOfInterest.push({'marker': marker, 'name': descriptionMarkers[i].name, 'description': descriptionMarkers[i].description});
     
+       marker.addListener('dragend', function() {
+        //console.log("HI");
+        //debugger;
+        contentString ='<div id="content"><h6>Point d\'int&eacute;r&ecirc;t #' + this.markerRefNum + '</h6><a onclick="deleteMarker(' + this.position.lat() + ', ' + this.position.lng() + ')"><i class="small material-icons" style="position: absolute;top: 1px;right: 15px;font-size: 13px;">delete</i></a>Name: ' + this.markerName + '<br>Location : (' + this.position.lat().toFixed(4) + ', ' + this.position.lng().toFixed(4) + ')</div>';
+        infowindow = new google.maps.InfoWindow({
+          content: contentString
+        });
+        infowindow.open(map, this);
+      });
       marker.addListener('click', function() {
+        console.log('I was here too');
+        toggleBounce(this);
         var contentString ='<div id="content"><h6>Point d\'int&eacute;r&ecirc;t #' + this.markerRefNum + '</h6><a onclick="deleteMarker(' + this.position.lat() + ', ' + this.position.lng() + ')"><i class="small material-icons" style="position: absolute;top: 1px;right: 15px;font-size: 13px;">delete</i></a>Name: ' + this.markerName + '<br>Location : (' + this.position.lat().toFixed(4) + ', ' + this.position.lng().toFixed(4) + ')</div>';
         var infowindow = new google.maps.InfoWindow({
            content: contentString
@@ -231,22 +238,17 @@ function compilePointsOfInterest() {
     }
   }
 }
-
 function handleNoGeolocation(errorFlag) {
   if (errorFlag) {
     var content = 'Error: The Geolocation service failed.';
   } else {
     var content = 'Error: Your browser doesn\'t support geolocation.';
   }
-
   var options = {
     map: map,
     position: new google.maps.LatLng(60, 105),
     content: content
   };
-
   var infowindow = new google.maps.InfoWindow(options);
   map.setCenter(options.position);
 }
-
-
